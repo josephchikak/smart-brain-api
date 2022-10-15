4205fd6b663d093fdb2d238e0c40d3eeb376e7e9
raytheboffin <111377374+josephchikak@users.noreply.github.com>
2022-10-15 15:06:43 +0300

    another database change

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const singin = require('./controllers/singin');
 const db = knex({
     client: 'pg',
     connection: {
-      host : process.env.DATABASE_URL,
+      connectionString : process.env.DATABASE_URL,
       ssl: true,
     }
   });
